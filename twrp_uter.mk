8a1e2f14dffa7154abd9a8c76f0d33495acdb01d
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-05 05:46:32 +0800

    uter: Set device name as PRODUCT_RELEASE_NAME
    It's useful when common tree needs referencing device name

--- twrp_uter.mk
+++ twrp_uter.mk
@@ -4,12 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_RELEASE_NAME := uter
+
 # Inherit from uter device
 $(call inherit-product, device/xiaomi/uter/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := uter
-PRODUCT_NAME := twrp_uter
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := UTER
