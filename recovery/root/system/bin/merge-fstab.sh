9d4e20cc5a74994f3097ef4f17594444b4590369
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-05 02:10:53 +0800

    uter: Merge device specific fstab with script

---
+++ recovery/root/system/bin/merge-fstab.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+echo >> /system/etc/recovery.fstab
+cat /system/etc/recovery.fstab.device >> /system/etc/recovery.fstab
+echo >> /system/etc/twrp.flags
+cat /system/etc/twrp.flags.device >> /system/etc/twrp.flags
