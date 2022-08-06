d341e28273ba862615d13a910e59dcd2480865e2
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-07 00:50:55 +0800

    mi8937: Add omni lunch target

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     twrp_mi8937-user \
     twrp_mi8937-userdebug \
     twrp_mi8937-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/omni_mi8937.mk
+
+COMMON_LUNCH_CHOICES += \
+    omni_mi8937-user \
+    omni_mi8937-userdebug \
+    omni_mi8937-eng
---
+++ omni_mi8937.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2022 Team Win Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_RELEASE_NAME := mi8937
+
+# Inherit from mi8937 device
+$(call inherit-product, device/xiaomi/mi8937/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := omni_$(PRODUCT_RELEASE_NAME)
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MSM8937
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
