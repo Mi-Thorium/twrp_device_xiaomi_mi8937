a4dea1addda0858a01b1267e5a302fcabf14ebd3
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-29 02:22:19 +0800

    mi8937: Import hvdcp_opti from daisy and enable for devices with qpnp-fg
    Fixes incorrect battery percentage on devices with qpnp-fg
    
    daisy package version: V11.0.21.0.QDLMIXM
    
    Change-Id: I234b6bf590b306a51ba8cbebdf97a4d66c3dce10

--- recovery/root/system/bin/determine-device.sh
+++ recovery/root/system/bin/determine-device.sh
@@ -36,14 +36,17 @@ case "$(cat /sys/firmware/devicetree/base/model)" in
 	"Qualcomm Technologies, Inc. MSM8937-PMI8950 QRD SKU1")
 		set_device_codename "land"
 		set_device_model "Redmi 3S / 3X"
+		start hvdcp_opti
 		;;
 	"Qualcomm Technologies, Inc. MSM8940-PMI8950 QRD SKU7")
 		set_device_codename "santoni"
 		set_device_model "Redmi 4X"
+		start hvdcp_opti
 		;;
 	"Qualcomm Technologies, Inc. MSM8937-PMI8950 MTP")
 		set_device_codename "prada"
 		set_device_model "Redmi 4 Standard"
+		start hvdcp_opti
 		;;
 esac
 
