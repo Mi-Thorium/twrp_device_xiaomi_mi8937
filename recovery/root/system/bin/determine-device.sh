d7facee7cb279f6d5050b2ce0ff5743da0e8d2f5
me-cafebabe <me.cafebabe@gmail.com>
2023-05-04 03:08:47 +0800

    Mi8937: Migrate to Mi8937v2 kernel
    Change-Id: Ibc4eaf7cb8834e1e8ed59f684f7fc406a88ba5c0

--- recovery/root/system/bin/determine-device.sh
+++ recovery/root/system/bin/determine-device.sh
@@ -7,47 +7,11 @@ workaround_gpio_flashlight() {
 	echo 0 > /sys/class/leds/flashlight/brightness;
 }
 
-case "$(cat /sys/firmware/devicetree/base/model)" in
-	"Qualcomm Technologies, Inc. MSM8917-PMI8937 QRD SKU5")
-		if [ -e /sys/class/leds/infrared/transmit ]; then
-			set_device_codename "rolex"
-			set_device_model "Redmi 4A"
-		else
-			set_device_codename "riva"
-			set_device_model "Redmi 5A (Nougat bootloader)"
-		fi
-		workaround_gpio_flashlight
-		;;
-	"Qualcomm Technologies, Inc. MSM8917 QRD SKU5")
-		set_device_codename "riva"
-		set_device_model "Redmi 5A (Oreo bootloader)"
-		workaround_gpio_flashlight
-		;;
-	"Qualcomm Technologies, Inc. MSM8917-PMI8937 MTP")
-		set_device_codename "ugglite"
-		set_device_model "Redmi Note 5A / Y1 Lite"
-		workaround_gpio_flashlight
-		;;
-	"Qualcomm Technologies, Inc. MSM8940-PMI8937 MTP")
-		set_device_codename "ugg"
-		set_device_model "Redmi Note 5A / Y1 Prime"
-		workaround_gpio_flashlight
-		;;
-	"Qualcomm Technologies, Inc. MSM8937-PMI8950 QRD SKU1")
-		set_device_codename "land"
-		set_device_model "Redmi 3S / 3X"
-		start hvdcp_opti
-		;;
-	"Qualcomm Technologies, Inc. MSM8940-PMI8950 QRD SKU7")
-		set_device_codename "santoni"
-		set_device_model "Redmi 4X"
-		start hvdcp_opti
-		;;
-	"Qualcomm Technologies, Inc. MSM8937-PMI8950 MTP")
-		set_device_codename "prada"
-		set_device_model "Redmi 4 Standard"
-		start hvdcp_opti
-		;;
-esac
+set_device_codename "$(cat /sys/xiaomi-msm8937-mach/codename)"
+set_device_model "$(cat /sys/xiaomi-msm8937-mach/product_name)"
+
+if [ "$(cat /sys/class/leds/flashlight/device/of_node/compatible)" == "qcom,leds-gpio-flash" ]; then
+	workaround_gpio_flashlight
+fi
 
 exit 0
